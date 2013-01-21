caf92b7e606cef639b18e21338da338bccd189c0
Faris <faris@theluckybead.com>
2013-01-21 12:35:50 -0800

    Added more information about virtualenv to the readme. Added a default wsgi.py conf

---
+++ project_name/project_name/wsgi.py
@@ -0,0 +1,6 @@
+import os
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "project_name.settings.local")
+
+from django.core.wsgi import get_wsgi_application
+application = get_wsgi_application()
