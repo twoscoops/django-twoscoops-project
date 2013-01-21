27797a61420a5014c8e83feb39b7bd9cf2f20597
Juan Riaza <juanriaza@gmail.com>
2013-01-21 14:21:05 +0100

    improvements

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -56,9 +56,6 @@ CACHES = {}
 ########## END CACHE CONFIGURATION
 
 
-
-
-
 ########## SECRET CONFIGURATION
 # See: https://docs.djangoproject.com/en/dev/ref/settings/#secret-key
 SECRET_KEY = get_env_setting('SECRET_KEY')
