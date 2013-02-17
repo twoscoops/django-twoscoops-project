302fdffa62f406ac827cfdf7fb49f35b19503826
Daniel Greenfeld <pydanny@gmail.com>
2013-02-17 11:43:02 -0800

    Add in test settings

---
+++ project_name/project_name/settings/test.py
@@ -0,0 +1,18 @@
+from base import *
+
+########## TEST SETTINGS
+TEST_RUNNER = 'discover_runner.DiscoverRunner'
+TEST_DISCOVER_TOP_LEVEL = SITE_ROOT
+TEST_DISCOVER_ROOT = SITE_ROOT
+TEST_DISCOVER_PATTERN = "*_tests.py"
+########## IN-MEMORY TEST DATABASE
+DATABASES = {
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": ":memory:",
+        "USER": "",
+        "PASSWORD": "",
+        "HOST": "",
+        "PORT": "",
+    },
+}
