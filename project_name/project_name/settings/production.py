5b89e8fa6b276948ea3312c20f09c8b66343cf13
Remco Wendt <remco.wendt@gmail.com>
2013-04-19 11:49:42 +0200

    Replaced tabs with spaces

--- project_name/project_name/settings/production.py
+++ project_name/project_name/settings/production.py
@@ -11,13 +11,13 @@ from django.core.exceptions import ImproperlyConfigured
 
 
 def get_env_setting(setting):
-	""" Get the environment setting or return exception """
-	try:
-		return environ[setting]
-	except KeyError:
-		error_msg = "Set the %s env variable" % setting
-		raise ImproperlyConfigured(error_msg)
-		
+    """ Get the environment setting or return exception """
+    try:
+        return environ[setting]
+    except KeyError:
+        error_msg = "Set the %s env variable" % setting
+        raise ImproperlyConfigured(error_msg)
+
 INSTALLED_APPS += ('gunicorn',)
 
 ########## EMAIL CONFIGURATION
