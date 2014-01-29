99079c4791d1aa26fce0c9f79b3a382966cbb5cc
Richard Barran <richard@arbee-design.co.uk>
2014-01-29 22:30:30 +0000

    Serve media files in dev - see also pull request #71.

--- project_name/project_name/urls.py
+++ project_name/project_name/urls.py
@@ -1,6 +1,7 @@
 from django.conf.urls import patterns, include, url
-from django.views.generic import TemplateView
+from django.conf.urls.static import static
 from django.conf import settings
+from django.views.generic import TemplateView
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
@@ -19,6 +20,9 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
 )
+
+# Uncomment the next line to serve media files in dev.
+# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
